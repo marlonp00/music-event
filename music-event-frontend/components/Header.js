51af5fe6dee706a1f1c90cd36ebddfb3510727d7
Marlon Perini <marlon.perini@gmail.com>
2022-08-29 15:55:21 +0200

    Added image source files

--- music-event-frontend/components/Header.js
+++ music-event-frontend/components/Header.js
@@ -3,7 +3,6 @@ import styles from "@/styles/Header.module.css"
 
 
 export default function Header() {
-
   
   return (
    <header className={styles.header}>
---
+++ music-event-frontend/components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/Showcase.module.css"
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Concert!</h1>
+      <h2>Finde die angesagtesten Konzerte in deiner Nähe</h2>
+    </div>
+  )
+}
