b17b848523ab8478d5a587c9f26d21f37cc4740c
Marlon Perini <marlon.perini@gmail.com>
2022-11-02 10:15:26 +0100

    Created Logic for Dashboard

--- music-event-frontend/pages/account/dashboard.js
+++ music-event-frontend/pages/account/dashboard.js
@@ -1,9 +1,43 @@
+import {parseCookies} from '@/helpers/index'
 import Layout from '@/components/Layout'
+import DashboardEvent from '@/components/DashboardEvent'
+import { API_URL } from '@/config/index'
+import styles from '@/styles/Dashboard.module.css'
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+
+  const deleteEvent = (id) => {
+    console.log(id);
+  }
+
   return (
     <Layout title='User Dashboard'>
+     <div className={styles.dash}>
       <h1>Dashboard</h1>
+      <h3>Meine Events</h3>
+      {events.map((event) => (
+        <DashboardEvent key={event.id} evt={event} handleDelete={deleteEvent} />
+      ))}
+     </div>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ req }) {
+  const {token} = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/api/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events
+    },
+  }
 }
